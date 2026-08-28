4c332d9da23d06237490a01ccf8878a484ee6591
Alberto Iong <albert0i@hotmail.com>
2026-08-28 09:59:34 +0800

    upper fix

--- seeds/todo_list.js
+++ seeds/todo_list.js
@@ -8,18 +8,18 @@ export async function seed(knex) {
 
   // Your array of todos
   const todos = [
-    { TITLE: 'Fix the quantum interference device broken by Stuart Bloom', STATUS: 'pending' },
-    { TITLE: 'Escape the repressive AI on the idyllic version of Earth', STATUS: 'pending' },
-    { TITLE: 'Enlist a powerful wizard to help Bert find a sorcery gift', STATUS: 'pending' },
-    { TITLE: 'Survive the post-apocalyptic Pasadena and avoid giant moths', STATUS: 'pending' },
-    { TITLE: 'Barter canned vegetables and cat food for rare comic books', STATUS: 'pending' },
-    { TITLE: 'Overthrow military dictator Barry Kripke in alternate reality', STATUS: 'pending' },
-    { TITLE: 'Locate Denise after she mysteriously disappears in the multiverse', STATUS: 'pending' },
-    { TITLE: 'Convince doctors in the mental institution that the multiverse is real', STATUS: 'pending' },
-    { TITLE: 'Break out of the Matrix pods before reality resets again', STATUS: 'pending' },
-    { TITLE: 'Undo the multiverse Armageddon accidentally unleashed by the gang', STATUS: 'pending' },
-    { TITLE: 'Help Gary secure his new job working for UPS', STATUS: 'pending' },
-    { TITLE: 'Find the original universe where Leonard and Sheldon live', STATUS: 'pending' }
+    { TITLE: 'Fix the quantum interference device broken by Stuart Bloom', STATUS: 'PENDING' },
+    { TITLE: 'Escape the repressive AI on the idyllic version of Earth', STATUS: 'PENDING' },
+    { TITLE: 'Enlist a powerful wizard to help Bert find a sorcery gift', STATUS: 'PENDING' },
+    { TITLE: 'Survive the post-apocalyptic Pasadena and avoid giant moths', STATUS: 'PENDING' },
+    { TITLE: 'Barter canned vegetables and cat food for rare comic books', STATUS: 'PENDING' },
+    { TITLE: 'Overthrow military dictator Barry Kripke in alternate reality', STATUS: 'PENDING' },
+    { TITLE: 'Locate Denise after she mysteriously disappears in the multiverse', STATUS: 'PENDING' },
+    { TITLE: 'Convince doctors in the mental institution that the multiverse is real', STATUS: 'PENDING' },
+    { TITLE: 'Break out of the Matrix pods before reality resets again', STATUS: 'PENDING' },
+    { TITLE: 'Undo the multiverse Armageddon accidentally unleashed by the gang', STATUS: 'PENDING' },
+    { TITLE: 'Help Gary secure his new job working for UPS', STATUS: 'PENDING' },
+    { TITLE: 'Find the original universe where Leonard and Sheldon live', STATUS: 'PENDING' }
   ];
 
   // Insert one by one (Oracle-compatible)
