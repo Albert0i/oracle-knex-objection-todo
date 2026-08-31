e9a86eb45f775019b6fa39e7d626aca4003cdd5f
Alberto Iong <albert0i@hotmail.com>
2026-08-31 17:04:07 +0800

    clean setting

--- knexfile.js
+++ knexfile.js
@@ -26,16 +26,6 @@ export default {
     debug: false,              // log SQL queries to console
     asyncStackTraces: false,   // show full async stack traces on errors
     fetchAsString: [ 'DATE', 'NUMBER' ], // return DATE/NUMBER columns as strings
-    // overly simplified snake_case -> camelCase converter
-    // postProcessResponse: (result, queryContext) => {
-    //   // TODO: add special case for raw results
-    //   // (depends on dialect)
-    //   if (Array.isArray(result)) {
-    //     return result.map((row) => convertToCamel(row));
-    //   } else {
-    //     return convertToCamel(result);
-    //   }
-    // },
   },
   /**
    * Production
@@ -50,21 +40,6 @@ export default {
     pool: { min: 2, max: 10 }
   }
 };
-
-// Convert snake_case or SNAKE_CASE keys to camelCase
-function convertToCamel(obj) {
-  if (!obj || typeof obj !== 'object') return obj;
-
-  return Object.entries(obj).reduce((acc, [key, value]) => {
-    // Lowercase the whole key first, then convert
-    const camelKey = key
-      .toLowerCase()
-      .replace(/_([a-z])/g, (_, c) => c.toUpperCase());
-
-    acc[camelKey] = value;
-    return acc;
-  }, {});
-}
 
 /**
  * Knex.js Installation
