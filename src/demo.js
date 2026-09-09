e3bc70863dd526d373b07b881a50cffbc6f5a49d
Alberto Iong <albert0i@hotmail.com>
2026-09-09 12:15:53 +0800

    add demo.js

---
+++ src/demo.js
@@ -0,0 +1,16 @@
+/**
+ * demo.js
+ */
+import basic from 'wwwbasic';
+
+basic.Basic(`
+PRINT "Hello World!"
+FOR i = 1 TO 10
+  PRINT "Counting "; i
+NEXT i
+`);
+
+/**
+ * wwwBASIC
+ * https://google.github.io/wwwbasic/
+ */
