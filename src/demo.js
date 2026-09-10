c2d6dbbf1d3d2208ff479d1e14eab410cfd585bf
Alberto Iong <albert0i@hotmail.com>
2026-09-10 10:39:20 +0800

    write more

--- src/demo.js
+++ src/demo.js
@@ -4,11 +4,15 @@
 import basic from 'wwwbasic';
 
 basic.Basic(`
-PRINT "Hello World!"
-FOR i = 1 TO 10
-  PRINT "Counting "; i
-NEXT i
-`);
+        PRINT "Hello World!"
+        FOR i = 1 TO 10
+          PRINT "Counting "; i
+        NEXT i
+        `, {
+              debug: true
+           });
+
+console.log('basic =', basic)
 
 /**
  * wwwBASIC
